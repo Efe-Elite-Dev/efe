47b3b217f5beee19ee6db61b0cbf722b8eb6f944
ELİTE <efe113878@gmail.com>
2026-05-16 12:57:02 +0300

    Update sky_subsystem.h

--- sky_subsystem.h
+++ sky_subsystem.h
@@ -2,17 +2,9 @@
 #define SKY_SUBSYSTEM_H
 
 #include <stdint.h>
-#include "ai_subsystem.h"
 
-static inline void run_sky_subsystem(int win_x, int win_y, uint32_t current_tick) {
-    gui_draw_rect(win_x + 15, win_y + 45, 470, 290, 0x0F172A);
-    gui_draw_rect(win_x + 35, win_y + 65, 30, 30, 0xFFB900);
-    gui_draw_rect(win_x + 80, win_y + 75, 120, 12, 0x38BDF8); 
+void gui_draw_rect(int start_x, int start_y, int width, int height, uint32_t color);
 
-    uint32_t fake_input_hash = (current_tick / 100); 
-    int current_intent = sky_ai_analyze_intent(fake_input_hash);
-
-    run_ai_core_render(win_x, win_y, current_tick, current_intent);
-}
+void run_sky_subsystem(int win_x, int win_y, uint32_t current_tick);
 
 #endif

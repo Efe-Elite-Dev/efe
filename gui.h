5000ecec8c69130eb16da16e0c1daf6ade4903d4
ELİTE <efe113878@gmail.com>
2026-05-16 21:13:30 +0300

    Update gui.h

--- gui.h
+++ gui.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 
+/* Wind OS Grafik Çizim Fonksiyonları Protokolleri */
 void gui_draw_rect(int x, int y, int width, int height, uint32_t color);
 void gui_refresh_desktop(void);
 

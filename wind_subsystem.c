887315104a07c29ec3fe3927ab5d6a8e7dfc1fd6
ELİTE <efe113878@gmail.com>
2026-05-17 00:19:23 +0300

    Update wind_subsystem.c

--- wind_subsystem.c
+++ wind_subsystem.c
@@ -1,8 +1,67 @@
-#include "wind_subsystem.h"
+#ifndef WIND_SUBSYSTEM_H
+#define WIND_SUBSYSTEM_H
 
-/* Wind OS Alt Sistem Durum Belirteci (1: Kurulum, 2: Canlı Masaüstü) */
-int wind_system_status = 1;
+#include <stdint.h>
 
-int get_wind_status(void) {
-    return wind_system_status;
+/* GRUB Multiboot Standart Yapısı */
+struct multiboot_info {
+    uint32_t flags;
+    uint32_t mem_lower;
+    uint32_t mem_upper;
+    uint32_t boot_device;
+    uint32_t cmdline;
+    uint32_t mods_count;
+    uint32_t mods_addr;
+    uint32_t syms[4];
+    uint32_t mmap_length;
+    uint32_t mmap_addr;
+    uint32_t drives_length;
+    uint32_t drives_addr;
+    uint32_t config_table;
+    uint32_t boot_loader_name;
+    uint32_t apm_table;
+    uint32_t vbe_control_info;
+    uint32_t vbe_mode_info;
+    uint16_t vbe_mode;
+    uint16_t vbe_interface_seg;
+    uint16_t vbe_interface_off;
+    uint16_t vbe_interface_len;
+    uint64_t framebuffer_addr;
+    uint32_t framebuffer_pitch;
+    uint32_t framebuffer_width;
+    uint32_t framebuffer_height;
+    uint8_t  framebuffer_bpp;
+    uint8_t  framebuffer_type;
+};
+
+/* Port G/Ç Saf Assembly Komutları */
+static inline void outb(uint16_t port, uint8_t data) {
+    asm volatile("outb %0, %1" : : "a"(data), "Nd"(port));
 }
+
+static inline uint8_t inb(uint16_t port) {
+    uint8_t ret;
+    asm volatile("inb %1, %0" : "=a"(ret) : "Nd"(port));
+    return ret;
+}
+
+/* ÇEKİRDEK VE GRAFİK SİSTEMLERİ */
+void init_idt(void);
+void clear_text_screen(void);
+void gui_refresh_desktop(void);
+void run_exe_subsystem(void);
+void swap_buffers(void);
+
+/* YAPAY ZEKA DESTEKLİ SÜRÜCÜ ODALARI */
+void init_mouse(void);
+void handle_mouse_polling(void);
+int ai_mouse_analyze_stress(void);
+
+void init_keyboard(void);
+void check_keyboard_pure(void);
+int ai_keyboard_analyze_cadence(void);
+
+/* MERKEZİ AI ORKESTRA ŞEFİ */
+int ai_core_predict_scheduler(int mouse_stress, int kb_cadence, int loop_count);
+
+#endif /* WIND_SUBSYSTEM_H */

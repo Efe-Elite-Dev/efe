64938a51e2b72689bcb1dcb4edf7dfe234ae5667
ELİTE <efe113878@gmail.com>
2026-05-16 12:55:43 +0300

    Update deb_subsystem.h

--- deb_subsystem.h
+++ deb_subsystem.h
@@ -3,9 +3,8 @@
 
 #include <stdint.h>
 
-static inline void run_deb_subsystem(int win_x, int win_y) {
-    gui_draw_rect(win_x + 15, win_y + 45, 470, 290, 0x212121);
-    gui_draw_rect(win_x + 40, win_y + 70, 40, 40, 0xD70A53);
-}
+void gui_draw_rect(int start_x, int start_y, int width, int height, uint32_t color);
+
+void run_deb_subsystem(int win_x, int win_y);
 
 #endif

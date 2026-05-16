93f2b06df2a170f5ac97dc9e180a3087afb47e05
ELİTE <efe113878@gmail.com>
2026-05-16 21:22:21 +0300

    Update wind_subsystem.c

--- wind_subsystem.c
+++ wind_subsystem.c
@@ -1,14 +1,8 @@
-#include "sky_subsystem.h"
+#include "wind_subsystem.h"
 
-uint32_t* vbe_vram = (uint32_t*)0xE0000000;
-uint32_t  vbe_pitch = SCREEN_WIDTH * 4;
-uint32_t  back_buffer[TOTAL_PIXELS];
-int setup_completed = 0;
+/* Wind OS Alt Sistem Durum Belirteci (1: Kurulum, 2: Canlı Masaüstü) */
+int wind_system_status = 1;
 
-// Linker çakışmalarını bitiren asıl tanımlamalar
-int active_window = 0;
-
-void run_sky_subsystem(void) {
-    // Okul versiyonu için ileride alt sistem alt yapısını burası tetikleyecek
-    setup_completed = 1;
+int get_wind_status(void) {
+    return wind_system_status;
 }

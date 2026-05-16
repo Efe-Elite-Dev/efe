ac7c2e61cd60244c53068b83d7687ed374021caa
ELİTE <efe113878@gmail.com>
2026-05-16 16:37:09 +0300

    Update boot.asm

--- boot.asm
+++ boot.asm
@@ -1,37 +1,39 @@
-bits 32
+MBOOT_PAGE_ALIGN    equ 1 << 0
+MBOOT_MEM_INFO      equ 1 << 1
+MBOOT_VIDEO_MODE    equ 1 << 2
+MBOOT_HEADER_MAGIC  equ 0x1BADB002
+MBOOT_HEADER_FLAGS  equ MBOOT_PAGE_ALIGN | MBOOT_MEM_INFO | MBOOT_VIDEO_MODE
+MBOOT_CHECKSUM      equ -(MBOOT_HEADER_MAGIC + MBOOT_HEADER_FLAGS)
+
+section .multiboot
+align 4
+    dd MBOOT_HEADER_MAGIC
+    dd MBOOT_HEADER_FLAGS
+    dd MBOOT_CHECKSUM
+    dd 0, 0, 0, 0, 0
+    dd 0
+    dd 800         ; Ekran Genişliği
+    dd 600         ; Ekran Yüksekliği
+    dd 32          ; Renk Derinliği (BPP)
+
 section .text
-        align 4
-        dd 0x1BADB002              ; Sihirli sayı (Multiboot)
-        dd 0x00000005              ; Flags (VBE Grafik modunu tetikler)
-        dd - (0x1BADB002 + 0x00000005)
-
-        ; VBE Grafik Modu Zorunlu İstekleri (800x600x32bpp LFB)
-        dd 0, 0, 0, 0, 0
-        dd 0                       ; Mode type (0 = LFB)
-        dd 800                     ; Width
-        dd 600                     ; Height
-        dd 32                      ; BPP
-
 global _start
+global load_idt    ; IDT yükleyicisini dışarıya açıyoruz
 extern kernel_main
-extern keyboard_handler_c
 
 _start:
-        cli
-        mov esp, stack_space
-        push ebx                   ; Multiboot info yapısının adresini C'ye gönder
-        call kernel_main
+    cli
+    mov esp, stack_space
+    push ebx       ; Multiboot info yapısını kernel_main'e parametre gönder
+    call kernel_main
+    jmp $
 
-global keyboard_handler_asm
-keyboard_handler_asm:
-        pushad
-        call keyboard_handler_c    ; kernel.c içindeki köprüyü tetikle
-        popad
-        iretd
+load_idt:
+    mov edx, [esp + 4]  ; C kodundan gelen IDT pointer adresini al
+    lidt [edx]          ; İşlemciye IDT'yi yükle
+    ret                 ; C koduna geri dön
 
 section .bss
-resb 8192
+align 16
 stack_space:
-
-; Modern Linker (.note.GNU-stack) uyarısını kökten çözen kritik satır
-section .note.GNU-stack noalloc noexec nowrite progbits
+    resb 8192      ; 8KB Kernel Stack Alanı

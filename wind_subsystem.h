e92905ac6042fe3108cf3b5cc03613ad7c2eb45b
ELİTE <efe113878@gmail.com>
2026-05-16 21:12:23 +0300

    Update wind_subsystem.h

--- wind_subsystem.h
+++ wind_subsystem.h
@@ -1,43 +1,50 @@
-#ifndef SKY_SUBSYSTEM_H
-#define SKY_SUBSYSTEM_H
+#ifndef WIND_SUBSYSTEM_H
+#define WIND_SUBSYSTEM_H
 
 #include <stdint.h>
 
-// === GLOBAL EKRAN YAPILANDIRMASI ===
-#define SCREEN_WIDTH     800
-#define SCREEN_HEIGHT    600
-#define TOTAL_PIXELS     (SCREEN_WIDTH * SCREEN_HEIGHT)
-
-// === KUTSAL DONANIM PORT KÖPRÜLERİ (APPLE HAL TARZI) ===
-static inline uint8_t inb(uint16_t port) {
-    uint8_t data;
-    __asm__ __volatile__("inb %1, %0" : "=a"(data) : "Nd"(port));
-    return data;
-}
-
-static inline void outb(uint16_t port, uint8_t data) {
-    __asm__ __volatile__("outb %0, %1" : : "a"(data), "Nd"(port));
-}
-
-// === MULTIBOOT VE VRAM PAYLAŞIMI ===
+/* Wind OS Multiboot Standart Grafik ve Donanım Bilgi Yapısı */
 struct multiboot_info {
-    uint32_t flags; uint32_t mem_lower; uint32_t mem_upper; uint32_t boot_device;
-    uint32_t cmdline; uint32_t mods_count; uint32_t mods_addr; uint32_t num;
-    uint32_t size; uint32_t addr; uint32_t shndx; uint32_t vbe_control_info;
-    uint32_t vbe_mode_info; uint16_t vbe_mode; uint16_t vbe_interface_seg;
-    uint16_t vbe_interface_off; uint16_t vbe_interface_len;
-    uint32_t framebuffer_addr; uint32_t framebuffer_pitch;
-    uint32_t framebuffer_width; uint32_t framebuffer_height;
+    uint32_t flags;
+    uint32_t mem_lower;
+    uint32_t mem_upper;
+    uint32_t boot_device;
+    uint32_t cmdline;
+    uint32_t mods_count;
+    uint32_t mods_addr;
+    uint32_t syms[4];
+    uint32_t mmap_length;
+    uint32_t mmap_addr;
+    uint32_t drives_length;
+    uint32_t drives_addr;
+    uint32_t config_table;
+    uint32_t boot_loader_name;
+    uint32_t apm_table;
+    uint32_t vbe_control_info;
+    uint32_t vbe_mode_info;
+    uint16_t vbe_mode;
+    uint16_t vbe_interface_seg;
+    uint16_t vbe_interface_off;
+    uint16_t vbe_interface_len;
+    uint64_t framebuffer_addr;
+    uint32_t framebuffer_pitch;
+    uint32_t framebuffer_width;
+    uint32_t framebuffer_height;
+    uint8_t  framebuffer_bpp;
     uint8_t  framebuffer_type;
-} __attribute__((packed));
+};
 
-extern uint32_t* vbe_vram;
-extern uint32_t  vbe_pitch;
-extern uint32_t  back_buffer[TOTAL_PIXELS];
-extern int setup_completed;
+/* Donanımsal I/O Port Bağlantıları */
+static inline void outb(uint16_t port, uint8_t data) {
+    asm volatile("outb %0, %1" : : "a"(data), "Nd"(port));
+}
 
-// === GUI VE ALT SİSTEM KÖPRÜLERİ ===
-extern int active_window;
-void run_sky_subsystem(void);
+static inline uint8_t inb(uint16_t port) {
+    uint8_t ret;
+    asm volatile("inb %1, %0" : "=a"(ret) : "Nd"(port));
+    return ret;
+}
 
-#endif // SKY_SUBSYSTEM_H
+int get_wind_status(void);
+
+#endif /* WIND_SUBSYSTEM_H */

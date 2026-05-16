060be2feabae33bd63923a7386d039f9104c9d4f
ELİTE <efe113878@gmail.com>
2026-05-16 21:16:37 +0300

    Update boot.asm

--- boot.asm
+++ boot.asm
@@ -18,12 +18,12 @@ align 4
     dd 0        
     dd 800      ; Çözünürlük Genişlik
     dd 600      ; Çözünürlük Yükseklik
-    dd 32       ; BPP Derinliği
+    dd 32       ; BPP Renk Derinliği
 
 section .bootstrap_stack, nobits
 align 16
 stack_bottom:
-    resb 16384
+    resb 16384  ; 16 KB Temiz Stack Alanı
 stack_top:
 
 section .text
@@ -32,48 +32,8 @@ extern kernel_main
 
 _start:
     mov esp, stack_top
-    push ebx
-    cli
-    call kernel_main
-
-.halt:
-    hlt
-    jmp .haltMBOOT_PAGE_ALIGN    equ 1 << 0
-MBOOT_MEM_INFO      equ 1 << 1
-MBOOT_GRAPHICS      equ 1 << 2
-MBOOT_HEADER_MAGIC  equ 0x1BADB002
-MBOOT_HEADER_FLAGS  equ MBOOT_PAGE_ALIGN | MBOOT_MEM_INFO | MBOOT_GRAPHICS
-MBOOT_CHECKSUM      equ -(MBOOT_HEADER_MAGIC + MBOOT_HEADER_FLAGS)
-
-section .multiboot
-align 4
-    dd MBOOT_HEADER_MAGIC
-    dd MBOOT_HEADER_FLAGS
-    dd MBOOT_CHECKSUM
-    dd 0
-    dd 0
-    dd 0
-    dd 0
-    dd 0
-    dd 0        
-    dd 800      ; Çözünürlük Genişlik
-    dd 600      ; Çözünürlük Yükseklik
-    dd 32       ; BPP Derinliği
-
-section .bootstrap_stack, nobits
-align 16
-stack_bottom:
-    resb 16384
-stack_top:
-
-section .text
-global _start
-extern kernel_main
-
-_start:
-    mov esp, stack_top
-    push ebx
-    cli
+    push ebx    ; Multiboot bilgi yapısını kernel_main'e aktar
+    cli         ; Kesmeleri kapat
     call kernel_main
 
 .halt:

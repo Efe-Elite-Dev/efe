202069004d90967371740586979305972293f277
ELİTE <efe113878@gmail.com>
2026-05-16 13:39:09 +0300

    Delete src directory

--- src/core/sky-init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Sky-OS Başlatılıyor..."
-
-# Arka planda yapay zeka asistan motorunu uyandır
-python3 /src/core/sky-assistant.py "Sistem başarıyla açıldı, kaptana selam çak!" &
-
-# Masaüstü ortamını (XFCE) ekrana bas
-xfce4-session

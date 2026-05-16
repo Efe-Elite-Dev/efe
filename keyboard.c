59cb1790ba2584ca45ec8d4289208d5391c91fdf
ELİTE <efe113878@gmail.com>
2026-05-16 21:08:44 +0300

    Add init_keyboard function implementation
    Implement init_keyboard function to enable IRQ1 for keyboard.

--- keyboard.c
+++ keyboard.c
@@ -1,6 +1,7 @@
-#ifndef KEYBOARD_H
-#define KEYBOARD_H
+#include "wind_subsystem.h"
+#include "keyboard.h"
 
-void init_keyboard(void);
-
-#endif /* KEYBOARD_H */
+void init_keyboard(void) {
+    /* Master PIC üzerinden IRQ1 (Klavye) Kesmesini Açma */
+    outb(0x21, inb(0x21) & ~(1 << 1));
+}

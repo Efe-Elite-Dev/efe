0eccca5705582906c41b1c84e86f599f77a36dec
ELİTE <efe113878@gmail.com>
2026-05-16 21:10:43 +0300

    Update ai_subsystem.h

--- ai_subsystem.h
+++ ai_subsystem.h
@@ -1,11 +1,6 @@
 #ifndef AI_SUBSYSTEM_H
 #define AI_SUBSYSTEM_H
 
-#include <stdint.h>
+int ai_predict_hardware_load(int mouse_delta_x, int loop_count);
 
-void gui_draw_rect(int start_x, int start_y, int width, int height, uint32_t color);
-
-int sky_ai_analyze_intent(uint32_t input_hash);
-void run_ai_core_render(int win_x, int win_y, uint32_t current_tick, int intent);
-
-#endif
+#endif /* AI_SUBSYSTEM_H */

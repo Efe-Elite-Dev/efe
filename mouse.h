c29cfb3bb8b33dcc93d40c998b02ef3ef3b014d1
ELİTE <efe113878@gmail.com>
2026-05-16 15:49:30 +0300

    Update mouse.h

--- mouse.h
+++ mouse.h
@@ -3,19 +3,26 @@
 
 #include <stdint.h>
 
-// Ekran çözünürlük tanımları (Kernel ile senkronize)
+// Ekran çözünürlük sınırları (mouse.c içindeki taşma kontrolleri için gerekli)
 #define SCREEN_WIDTH     800
 #define SCREEN_HEIGHT    600
 
-// Dışarıya aktarılan global fare koordinatları
+// mouse.c içindeki global fare değişkenleri
 extern int mouse_x;
 extern int mouse_y;
+extern int mouse_left_click;
+extern int mouse_right_click;
+extern int mouse_middle_click;
 
-// Fonksiyon prototipleri
+// Fare okunun görsel matrisi (Sprite)
+extern const uint8_t mouse_pointer_sprite[19][12];
+
+// kernel.c tarafından çağrılacak fonksiyon prototipleri
 void init_mouse(void);
 void handle_mouse_polling(void);
 
-// Fare okunun 12x19 matrisi (Sprite)
-extern const uint8_t mouse_pointer_sprite[19][12];
+// Alt seviye port fonksiyonları
+unsigned char inb(unsigned short port);
+void outb(unsigned short port, unsigned char data);
 
-#endif
+#endif // MOUSE_H

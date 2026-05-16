817d0c16aca1378b60f22d2bafeb8f825c154049
ELİTE <efe113878@gmail.com>
2026-05-16 21:08:00 +0300

    Update screen.h

--- screen.h
+++ screen.h
@@ -1,8 +1,6 @@
 #ifndef SCREEN_H
 #define SCREEN_H
 
-void sky_clear_screen(void);
-void sky_print(const char* str);
-void sky_put_char(char c);
+void clear_text_screen(void);
 
-#endif
+#endif /* SCREEN_H */

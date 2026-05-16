ed93c29828453959ce0abcf84c9e2d9da4cb8980
ELİTE <efe113878@gmail.com>
2026-05-16 04:32:16 +0300

    Update exe_subsystem.h

--- exe_subsystem.h
+++ exe_subsystem.h
@@ -3,13 +3,8 @@
 
 #include <stdint.h>
 
-void gui_draw_rect(int start_x, int start_y, int width, int height, uint32_t color);
-
 static inline void run_exe_subsystem(int win_x, int win_y) {
-    // İç panel arka planı
     gui_draw_rect(win_x + 15, win_y + 45, 470, 290, 0xF3F3F3);
-    
-    // Windows Logosunu Temsil Eden Modern Dörtlü Mavi Kareler
     gui_draw_rect(win_x + 50, win_y + 80, 22, 22, 0x0078D4); 
     gui_draw_rect(win_x + 75, win_y + 80, 22, 22, 0x0078D4); 
     gui_draw_rect(win_x + 50, win_y + 105, 22, 22, 0x0078D4); 

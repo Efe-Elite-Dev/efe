07c759f3c98fb8c80f890be0788c38a6cd1186ac
ELİTE <efe113878@gmail.com>
2026-05-16 21:06:18 +0300

    Update deb_subsystem.h

--- deb_subsystem.h
+++ deb_subsystem.h
@@ -1,10 +1,6 @@
 #ifndef DEB_SUBSYSTEM_H
 #define DEB_SUBSYSTEM_H
 
-#include <stdint.h>
+void process_deb_package(void);
 
-void gui_draw_rect(int start_x, int start_y, int width, int height, uint32_t color);
-
-void run_deb_subsystem(int win_x, int win_y);
-
-#endif
+#endif /* DEB_SUBSYSTEM_H */
